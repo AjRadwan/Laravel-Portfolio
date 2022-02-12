a58305e5d9bef6228ff9bbe4c81c63392abcbaf8
Anowar <1hafizajr@gmail.com>
2022-02-12 10:09:59 +0600

    added controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\PhotoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('photos', PhotoController::class);
+Route::resource('galleries', PhotoController::class);
